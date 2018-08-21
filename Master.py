206a88e9e87dbf4ded25df0ff0a5dd89cc6f0b1c
Sept-killmenow <39297761+Sept-killmenow@users.noreply.github.com>
2018-08-21 12:32:56 +1200

    Cleaned up code + removed excess variables

--- Master.py
+++ Master.py
@@ -1,52 +1,43 @@
-'#UI'
-nav = ''
+# UI
 Start = 0
 Exit = 0
 ui = '='
 
-#Class Variables
+# Class Variables
 xp = 0
 currency = 100
 health = 0
 armor = 0
 mobility = 0
 
-#Text references
-back = 'back'
-Back = 'Back'
-backI = ''
-Instructions = ('Instructions')
-instructions = ('instructions')
-Class= ''
-
-#loop variables
-loopClass = 0
-ClassCheck = ''
+# Input references
+Class = ''
 
 
 def menu():
-        backI = '0'
-        print(ui*20)
-        print()
-        print(
-            "      ARA RPG   "
-        )
-        print()
-        print(ui*20)
-        print()
-        print(
-            "       Start    "
-        )
-        print(
-            "    Instructions "
-        )
-        print(
-            "       Exit"
-        )
-        print()
-        print(ui*20)
 
-def loopMenu():
+        print(ui*30)
+        print()
+        print(
+            "           ARA RPG   "
+        )
+        print()
+        print(ui*30)
+        print()
+        print(
+            "            Start    "
+        )
+        print(
+            "         Instructions "
+        )
+        print(
+            "            Exit"
+        )
+        print()
+        print(ui*30)
+
+
+def loopmenu():
 
     while Exit != 1:
         menu()
@@ -57,19 +48,19 @@ def loopMenu():
         elif nav == 'Start':
             break
 
-
         if nav == 'Instructions':
-            InstructionS()
+            instructions()
             break
 
         elif nav == 'instructions':
-            InstructionS()
+            instructions()
             break
 
-def InstructionS():
+
+def instructions():  # Loop that displays the games instructions until the user types Back
     while Exit != 1:
         print()
-        print(ui*20)
+        print(ui*30)
         print()
         print(
             "1. To navigate menus and UI you shall type out what the option is not click on it"
@@ -81,45 +72,84 @@ def InstructionS():
             "3. Have fun"
         )
         print()
-        print(ui*20)
+        print(ui*30)
         print('If you would like to return type back')
-        backI = input('...')
-        if backI == back:
-            loopMenu()
+        back = input('...')
+        if back == back:
+            loopmenu()
             break
 
-        elif backI == Back:
-            loopMenu()
+        elif back == Back:
+            loopmenu()
             break
         print()
         print()
         print()
 
-def playerStats():
 
-    print (ui * 10, player.lstrip(), ui * 10)
+def playerstats():
+
+    print(ui * 15, player.lstrip(), ui * 15)
     print("Health", health)
     print("Armor", armor)
     print("Mobility", mobility)
     count = len(player)
-    print (ui * count + ui * 22)
+    print(ui * count + ui * 32)
 
 
+def townhall():
+    print(ui*30)
+    print()
+    print(
+        'You enter the town hall and'
+        )
+    print(
+        ' see a notice board and a '
+        )
+    print(
+        'Receptionist. what do you do:'
+        )
+    print()
+    print('1. Check the notice board')
+    print('2. Talk to the Receptionist')
+    print('3. Go back to the Town square')
+    print(ui*30)
+    print()
+    print('Use your number keys to choose')
 
 
-loopMenu()
+def woke():
+    print(
+        'You wake up in the middle '
+
+    )
+    print(
+        'of a town square, surrounded'
+    )
+    print(
+        'by a large market a Town hall.'
+    )
+    print()
+    print('Where would you like to go:')
+    print('1. The Market')
+    print('2. The Town Hall')
+    print()
+    print('Use your number keys to choose')
+
+
+loopmenu()
 player = input(
     "Please enter your name..."
 )
 while Exit != 1:
-    print(ui * 26)
+    print(ui * 30)
     print(
         "  Please Select a class"
     )
     print(
         " [Warrior] [Tank] [Scout]"
     )
-    print(ui * 26)
+    print(ui * 30)
     Class = input("...")
     if Class == 'Warrior':
         health = 125
@@ -156,39 +186,27 @@ while Exit != 1:
         armor = 100
         mobility = 100
         break
-playerStats()
-print()
-print(ui*25)
-print()
-print(
-      'You wake up in the middle '
-
-)
-print(
-    'of a town square, surrounded'
-)
-print(
-    'by a large market a Town hall.'
-)
-print()
-print('Where would you like to go:')
-print('1. The Market')
-print('2. The Town Hall')
-print()
-print('Use your number keys to choose')
-while Exit != 1:
+playerstats()
+woke()
+while Exit != 1:  # Choices from Woke() loop
     choice = input('...')
     print()
-    print(ui*25)
     if choice == '1':
         print('jeff')
-        #Market()
+        # market()
         break
     if choice == '2':
-        print('jeff')
-        #townHall()
+        townhall()
         break
-
-
-
-
+while Exit != 1:  # Choices from townHall() loop
+    choice = input('...')
+    print()
+    if choice == '1':
+        # noticeboard
+        break
+    if choice == '2':
+        # receptionist()
+        break
+    if choice == '3':
+        # woke loop()
+        break
