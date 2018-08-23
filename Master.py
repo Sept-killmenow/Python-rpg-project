775e5e7cd65b2335da9f8325bbc6bceec7f66acc
Sept-killmenow <39297761+Sept-killmenow@users.noreply.github.com>
2018-08-23 23:37:36 +1200

    Inventory Cont and color Added

--- Master.py
+++ Master.py
@@ -2,6 +2,39 @@
 Start = 0
 Exit = 0
 ui = '='
+class bcolors:
+    # Regular Colors
+    Black = "\033[0;30m"  # Black
+    Red = "\033[0;31m"  # Red
+    Green = "\033[0;32m"  # Green
+    Yellow = "\033[0;33m"  # Yellow
+    Blue = "\033[0;34m"  # Blue
+    Purple = "\033[0;35m"  # Purple
+    Cyan = "\033[0;36m"  # Cyan
+    White = "\033[0;37m"  # White
+
+    # Underline
+    UBlack = "\033[4;30m"  # Black
+    URed = "\033[4;31m"  # Red
+    UGreen = "\033[4;32m"  # Green
+    UYellow = "\033[4;33m"  # Yellow
+    UBlue = "\033[4;34m"  # Blue
+    UPurple = "\033[4;35m"  # Purple
+    UCyan = '\033[4;36m'  # Cyan
+    UWhite = "\033[4;37m"  # White
+
+    # Background
+    On_Black = "\033[40m"  # Black
+    On_Red = "\033[41m"  # Red
+    On_Green = "\033[42m"  # Green
+    On_Yellow = "\033[43m"  # Yellow
+    On_Blue = "\033[44m"  # Blue
+    On_Purple = "\033[45m"  # Purple
+    On_Cyan = "\033[46m"  # Cyan
+    On_White = "\033[47m"  # White
+
+
+
 
 # Class Variables
 xp = 0
@@ -16,6 +49,26 @@ Class = ''
 choice = ''
 
 # Inventory
+weapons_buy = [
+    '1. Sharpened Iron Sword $150', '2. Iron Axe $200', '3. Sharpened Iron Axe $250', '4. Steel Sword $550',
+    '5. Sharpened Steel Sword $750', '6. Steel Axe $1000', '7. Sharpened Steel Axe $1500',
+    '8. Obsidian Strong Sword $2500', '9. Obsidian Rapier $2500', '10.Obsidian Axe $2500'
+]
+weapons_buy_scout = [
+    '1. Sharpened Iron Dagger $150', '2. Iron Short Sword $200', '3. Steel Dagger $400',
+    '4. Sharpened Steel Dagger $650', '5. Steel Short Sword $500', '6. Sharpened Steel Short Sword $750',
+    '7. Obsidian Dagger', 'Obsidian Rapier'
+]
+potions_buy = [
+    '11. Small Health Potion $75', '12. Medium Health Potion $125', '13. Large Health Potion $200',
+    '14. Mobility Buff Vial $750', '15. Health Buff Vial $750'
+]
+potions_buy_scout = [
+    '11. Small Health Potion $75', '12. Medium Health Potion $125', '13. Large Health Potion $200',
+    '14. Mobility Buff Vial $750', '15. Health Buff Vial $750'
+]
+misc_buy = []
+misc_buy_scout = []
 weapons = []
 potions = ['Small Health Potion', 'Small Health Potion', 'Small Health Potion']
 misc = []
@@ -25,14 +78,8 @@ inventory_count = len(inventory)
 
 # Weapons
 
+
 # Potions
-
-
-def lhp():
-    if health <= 100:
-        health = health + 100
-    if health >= 100:
-        health = 100
 
 
 # Misc
@@ -40,7 +87,7 @@ def lhp():
 
 def menu():
 
-        print(ui*30)
+        print(bcolors.UCyan + ui*30)
         print()
         print(
             "           ARA RPG   "
@@ -196,7 +243,7 @@ def woke_loop():
 def inventory():
     inventory = weapons + potions + misc
     inventory_count = len(inventory)
-    inventory_count # This should have no effect but it makes the inventory_count work so I dont HECKING know anymore
+    inventory_count    # This should have no effect but it makes the inventory_count work so I dont HECKING know anymore
     print('=============== Inventory ===============')
     print('Weapons:')
     for weapon in weapons:
@@ -218,8 +265,30 @@ def shop():
     print('How do you do stranger would you like to buy anything?')
     print('You have', currency, 'dollars')
     print('Weapons:')
+    if Class == 'Warrior':
+        for weapon_buy_1 in weapons_buy:
+            print(weapon_buy_1)
+    if Class == 'warrior':
+        for weapon_buy_1 in weapons_buy:
+            print(weapon_buy_1)
+    if Class == 'Tank':
+        for weapon_buy_1 in weapons_buy:
+            print(weapon_buy_1)
+    if Class == 'tank':
+        for weapon_buy_1 in weapons_buy:
+            print(weapon_buy_1)
+    if Class == 'Scout':
+        for weapon_buy_2 in weapons_buy_scout:
+            print(weapon_buy_2)
+    if Class == 'scout':
+        for weapon_buy_2 in weapons_buy_scout:
+            print(weapon_buy_2)
     print('Potions:')
+    for potion_buy in potions_buy:
+        print(potion_buy)
     print('Misc:')
+    for miscs_buy in misc_buy:
+        print(miscs_buy)
     print('====================================')
     print('If you would like to buy something')
     print('Or if you would like to return to the Town square type back')
